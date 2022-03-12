c2ce81b44f4879fed03471945dd032c1bef11aa0
Carlos Vitorino <carlos.vitorino@sitana.pt>
2022-03-12 17:48:39 +0000

    allow login

--- server/core/_service_config.js
+++ server/core/_service_config.js
@@ -6,6 +6,17 @@ if (_env.is("dev")) {
     _service.allow()
 }
 
+/**
+ * Netuno JWT Authorization Service
+ */
+ if (_service.path == '_auth') {
+    _service.allow()
+}
+
+if (_service.path == 'Index.netuno?action=login') {
+    _service.allow()
+}
+
 /*
 if (_service.path == 'samples/my-service') {
     _service.allow()
