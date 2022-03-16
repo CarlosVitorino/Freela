480f290e7daad3f41d56da99ede730272e2db5f0
Carlos Vitorino <carlos.vitorino@sitana.pt>
2022-03-16 15:48:38 +0000

    git file

--- website/git.sh
+++ website/git.sh
@@ -8,7 +8,10 @@ BASE=$(git merge-base @ "$UPSTREAM")
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
 elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
+    echo "Will pull"
+    git pull
+    echo "Will build"
+    ./fastBuild.sh
 elif [ $REMOTE = $BASE ]; then
     echo "Need to push"
 else
